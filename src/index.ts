8d846990d2848e7475d7456f0da55c21c0b90e53
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-05 14:28:38 -0400

    Extract server creation to www file in bin

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,10 @@ import usersRouter from "./routes/users";
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT;
-
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
 });
 
 app.use("/users", usersRouter);
 
-app
-  .listen(PORT, () => {
-    console.log(`Server running on PORT: ${PORT}`);
-  })
-  .on("error", (error) => {
-    throw new Error(error.message);
-  });
+export default app;
