1ba3ba688d927f304214f9840adb576eb505591b
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-05 16:49:13 -0400

    Add basic routes and controls for pets

--- src/controllers/petController.ts
+++ src/controllers/petController.ts
@@ -4,6 +4,42 @@ const pets_get = asyncHandler(async (req, res) => {
   res.json({ petCount: 0, userPets: 0 });
 });
 
-const petController = { pets_get };
+const pet_get = asyncHandler(async (req, res) => {
+  res.json({
+    pet: {
+      id: req.params.id,
+      name: "specifipet",
+      age: "0 hours",
+      health: 100,
+      mood: 100,
+    },
+  });
+});
+
+const pet_create = asyncHandler(async (req, res) => {
+  res.json({
+    pet: {
+      id: 1,
+      name: "coolpet",
+      age: "0 hours",
+      health: 100,
+      mood: 100,
+    },
+  });
+});
+
+const pet_update = asyncHandler(async (req, res) => {
+  res.json({
+    pet: {
+      id: req.params.id,
+      name: "updatedpet",
+      age: "0 hours",
+      health: 100,
+      mood: 100,
+    },
+  });
+});
+
+const petController = { pets_get, pet_get, pet_create, pet_update };
 
 export default petController;
--- src/routes/pets.ts
+++ src/routes/pets.ts
@@ -4,4 +4,10 @@ const petsRouter = express.Router();
 
 petsRouter.get("/", petController.pets_get);
 
+petsRouter.get("/:id", petController.pet_get);
+
+petsRouter.post("/", petController.pet_create);
+
+petsRouter.put("/:id", petController.pet_update);
+
 export default petsRouter;
