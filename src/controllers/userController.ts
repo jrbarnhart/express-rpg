318889c53eb27373a97d5700f27ada346cc917e2
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-08 13:51:21 -0400

    Add zod schema for new users

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,4 +1,33 @@
 import asyncHandler from "express-async-handler";
+import { z } from "zod";
+
+const NewUser = z.object({
+  email: z
+    .string()
+    .trim()
+    .email({
+      message: "Must be a valid email format. Example: user@service.com",
+    })
+    .min(5, { message: "Email must be at least 5 characters long" })
+    .max(320, { message: "Email must be at most 320 characters long" }),
+  username: z
+    .string()
+    .trim()
+    .min(5, { message: "Username must be at least 5 characters long" })
+    .max(64, { message: "Username must be at most 64 characters long" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(100, { message: "Password must be at most 100 characters long" })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number" })
+    .regex(/^\S*$/, { message: "Password must not contain spaces" }),
+});
 
 const users_list = asyncHandler(async (req, res) => {
   res.json({ userCount: 0, userPets: 0 });
