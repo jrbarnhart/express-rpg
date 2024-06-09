ef801adc716741d3380554ec5a90957c1438d9ac
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-09 00:24:05 -0400

    Add currentUser to res.locals

--- src/app.ts
+++ src/app.ts
@@ -29,6 +29,11 @@ app.use(
 );
 app.use(passport.session());
 
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 // Routes
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
