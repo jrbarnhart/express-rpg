f241ca2698590dc68cfdd0effc4b0a71788379a9
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-09 00:07:51 -0400

    Reorder routes so specific routes are matched first

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,16 +2,14 @@ import express from "express";
 const usersRouter = express.Router();
 import userController from "../controllers/userController";
 
-usersRouter.get("/", userController.users_list);
-
-usersRouter.get("/:id", userController.user_get);
-
-usersRouter.post("/", userController.user_create);
-
-usersRouter.put("/:id", userController.user_update);
+usersRouter.get("/login-fail", userController.user_login_fail);
 
 usersRouter.post("/login", userController.user_login);
 
-usersRouter.get("/login-fail", userController.user_login_fail);
+usersRouter.get("/:id", userController.user_get);
+usersRouter.put("/:id", userController.user_update);
+
+usersRouter.get("/", userController.users_list);
+usersRouter.post("/", userController.user_create);
 
 export default usersRouter;
