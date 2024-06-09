f9ef58921c76811e808b1dd4bf028bc3b0b4b107
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-09 18:21:22 -0400

    Add console log messages for debugging

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -188,6 +188,7 @@ const user_update = asyncHandler(async (req, res, next) => {
   }
 
   if (validatedData.data.password) {
+    console.log("Password in data path");
     bcrypt.hash(
       validatedData.data.password,
       10,
@@ -216,8 +217,10 @@ const user_update = asyncHandler(async (req, res, next) => {
             email: updatedUser.email,
             username: updatedUser.username,
           };
+          console.log("Sent password in data res");
           res.json(responseJSON);
         } catch (error) {
+          console.log("Error path for password  in data");
           responseJSON.success = false;
           responseJSON.message = "User update failed.";
 
@@ -232,7 +235,8 @@ const user_update = asyncHandler(async (req, res, next) => {
         }
       }
     );
-  } else if (!validatedData.data.password) {
+  } else {
+    console.log("No password in data path");
     try {
       const updatedUser = await prisma.user.update({
         where: { username: req.user?.username },
