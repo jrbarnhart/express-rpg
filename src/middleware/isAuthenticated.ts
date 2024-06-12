776042d09a9467f51855093a262d62650e4f7df7
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-12 18:39:06 -0400

    Add optional admin arg

--- src/middleware/isAuthenticated.ts
+++ src/middleware/isAuthenticated.ts
@@ -2,23 +2,27 @@ import { Request, Response, NextFunction } from "express";
 import passport, { AuthenticateCallback } from "passport";
 import { iResponseJSON } from "../lib/types";
 
-const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
-  const callback: AuthenticateCallback = (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      const responseJSON: iResponseJSON = {
-        success: false,
-        message: "Authorization required. Check credentials and try again.",
-      };
-      res.json(responseJSON);
-      return;
-    }
-    req.user = user;
-    next();
+const isAuthenticated = (isAdmin: boolean = false) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const callback: AuthenticateCallback = (err, user) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        const responseJSON: iResponseJSON = {
+          success: false,
+          message: "Authorization required. Check credentials and try again.",
+        };
+        res.json(responseJSON);
+        return;
+      }
+      if (isAdmin) {
+        // Do stuff
+      }
+      req.user = user;
+      next();
+    };
+    passport.authenticate("jwt", { session: false }, callback)(req, res, next);
   };
-  passport.authenticate("jwt", { session: false }, callback)(req, res, next);
 };
-
 export default isAuthenticated;
