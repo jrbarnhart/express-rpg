87a39115a894aae1925ce25f54c704aecc05cebd
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-13 17:32:57 -0400

    Add handler for getting list of all species

--- src/controllers/speciesController.ts
+++ src/controllers/speciesController.ts
@@ -1,4 +1,12 @@
-const species_list = () => {};
+import asyncHandler from "express-async-handler";
+import prisma from "../lib/prisma";
+import sendResponse from "../lib/sendResponse";
+
+const species_list = asyncHandler(async (req, res) => {
+  const allSpecies = await prisma.species.findMany();
+
+  sendResponse(res, "All species retrieved.", allSpecies);
+});
 
 const species_create = () => {};
 
