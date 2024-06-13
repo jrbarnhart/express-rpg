0b4388e676c901c24607b8aca91fa2b9313d78dd
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-13 18:02:10 -0400

    Add controller method for creating species

--- src/controllers/speciesController.ts
+++ src/controllers/speciesController.ts
@@ -1,6 +1,10 @@
 import asyncHandler from "express-async-handler";
 import prisma from "../lib/prisma";
 import sendResponse from "../lib/sendResponse";
+import validateRequestData from "../lib/zod/validateRequestData";
+import { SpeciesSchema } from "../lib/zod/Species";
+import formatPrismaError from "../lib/formatPrismaError";
+import sendErrorResponse from "../lib/sendErrorResponse";
 
 const species_list = asyncHandler(async (req, res) => {
   const allSpecies = await prisma.species.findMany();
@@ -8,7 +12,30 @@ const species_list = asyncHandler(async (req, res) => {
   sendResponse(res, "All species retrieved.", allSpecies);
 });
 
-const species_create = () => {};
+const species_create = asyncHandler(async (req, res) => {
+  const data = validateRequestData(req.body.data, res, SpeciesSchema);
+
+  if (!data) return;
+
+  try {
+    const newSpecies = await prisma.species.create({
+      data: {
+        name: data.name,
+        colors: { connect: data.colorIds.map((id) => ({ id })) },
+      },
+    });
+
+    sendResponse(res, "New species added.", newSpecies);
+  } catch (error) {
+    const formattedPrismaError = formatPrismaError(error);
+    console.log(error);
+    sendErrorResponse(
+      res,
+      "Error adding species to database.",
+      formattedPrismaError
+    );
+  }
+});
 
 const speciesController = { species_list, species_create };
 
