6a67ed64f16bb7dae9fe3a790a5a62c0b18a07ec
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-13 19:12:08 -0400

    Add route for updating colors

--- src/routes/colors.ts
+++ src/routes/colors.ts
@@ -3,6 +3,12 @@ import colorController from "../controllers/colorController";
 import isAuthenticated from "../middleware/isAuthenticated";
 const colorsRouter = express.Router();
 
+colorsRouter.put(
+  "/:id",
+  isAuthenticated("ADMIN"),
+  colorController.color_update
+);
+
 colorsRouter.get("/", isAuthenticated("ADMIN"), colorController.colors_list);
 
 colorsRouter.post("/", isAuthenticated("ADMIN"), colorController.color_create);
