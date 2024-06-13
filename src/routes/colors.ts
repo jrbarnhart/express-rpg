4c3f8d980b822c67ad1ba46c12bd8d0139d294c6
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-13 17:15:35 -0400

    Add route for getting list of all colors

--- src/routes/colors.ts
+++ src/routes/colors.ts
@@ -3,6 +3,8 @@ import colorController from "../controllers/colorController";
 import isAuthenticated from "../middleware/isAuthenticated";
 const colorsRouter = express.Router();
 
+colorsRouter.get("/", isAuthenticated("ADMIN"), colorController.colors_list);
+
 colorsRouter.post("/", isAuthenticated("ADMIN"), colorController.color_create);
 
 export default colorsRouter;
