a73fda54e2ff27b3b2f13fb7cbd8165ce50a3723
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-14 18:55:27 -0400

    Add handlers for updating and getting species

--- src/controllers/speciesController.ts
+++ src/controllers/speciesController.ts
@@ -2,13 +2,28 @@ import asyncHandler from "express-async-handler";
 import prisma from "../lib/prisma/prisma";
 import sendResponse from "../lib/controllerUtils/sendResponse";
 import validateRequestData from "../lib/zod/validateRequestData";
-import { CreateSpeciesSchema } from "../lib/zod/Species";
+import { CreateSpeciesSchema, UpdateSpeciesSchema } from "../lib/zod/Species";
 import handlePrismaError from "../lib/prisma/handlePrismaError";
+import sendErrorResponse from "../lib/controllerUtils/sendErrorResponse";
 
 const species_list = asyncHandler(async (req, res) => {
-  const allSpecies = await prisma.species.findMany();
+  const allSpecies = await prisma.species.findMany({
+    include: { colors: { select: { id: true, name: true } } },
+  });
 
   sendResponse(res, "All species retrieved.", allSpecies);
+});
+
+const species_get = asyncHandler(async (req, res) => {
+  const species = await prisma.species.findUnique({
+    where: { id: parseInt(req.params.id) },
+    include: { colors: { select: { id: true, name: true } } },
+  });
+  if (!species) {
+    sendErrorResponse(res, "Species not found.");
+  } else {
+    sendResponse(res, "Species retrieved successfully.", species);
+  }
 });
 
 const species_create = asyncHandler(async (req, res) => {
@@ -30,6 +45,29 @@ const species_create = asyncHandler(async (req, res) => {
   }
 });
 
-const speciesController = { species_list, species_create };
+const species_update = asyncHandler(async (req, res) => {
+  const data = validateRequestData(req.body.data, res, UpdateSpeciesSchema);
+
+  if (!data) return;
+
+  try {
+    const updatedSpecies = await prisma.species.update({
+      where: { id: parseInt(req.params.id) },
+      data: { ...data },
+      include: { colors: { select: { id: true, name: true } } },
+    });
+
+    sendResponse(res, "Species updated successfully.", updatedSpecies);
+  } catch (error) {
+    handlePrismaError(error, res, "Error while updating species.");
+  }
+});
+
+const speciesController = {
+  species_list,
+  species_get,
+  species_create,
+  species_update,
+};
 
 export default speciesController;
