260f6202944f378e476a02abeddf93b5e9defba0
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-14 14:51:50 -0400

    Add routes for npc template

---
+++ src/routes/npcTemplates.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import npcTemplateController from "../controllers/npcTemplateController";
+import isAuthenticated from "../middleware/isAuthenticated";
+const npcTemplateRouter = express.Router();
+
+npcTemplateRouter.get(
+  "/:id",
+  isAuthenticated("ADMIN"),
+  npcTemplateController.npc_template_get
+);
+
+npcTemplateRouter.put(
+  "/:id",
+  isAuthenticated("ADMIN"),
+  npcTemplateController.npc_template_update
+);
+
+npcTemplateRouter.get(
+  "/",
+  isAuthenticated("ADMIN"),
+  npcTemplateController.npc_templates_list
+);
+
+npcTemplateRouter.post(
+  "/",
+  isAuthenticated("ADMIN"),
+  npcTemplateController.npc_template_create
+);
