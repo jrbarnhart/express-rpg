22163f53b7c6d8d7835ce09794881365081512bc
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-14 20:03:23 -0400

    Add method for getting a color

--- src/controllers/colorController.ts
+++ src/controllers/colorController.ts
@@ -4,11 +4,24 @@ import { CreateColorSchema, UpdateColorSchema } from "../lib/zod/Color";
 import prisma from "../lib/prisma/prisma";
 import sendResponse from "../lib/controllerUtils/sendResponse";
 import handlePrismaError from "../lib/prisma/handlePrismaError";
+import sendErrorResponse from "../lib/controllerUtils/sendErrorResponse";
 
 const colors_list = asyncHandler(async (req, res) => {
   const allColors = await prisma.color.findMany();
 
   sendResponse(res, "All colors retrieved.", allColors);
+});
+
+const color_get = asyncHandler(async (req, res) => {
+  const color = await prisma.color.findUnique({
+    where: { id: parseInt(req.params.id) },
+  });
+
+  if (!color) {
+    sendErrorResponse(res, "Color not found.");
+  } else {
+    sendResponse(res, "Color retrieved successfully.", color);
+  }
 });
 
 const color_create = asyncHandler(async (req, res) => {
@@ -48,6 +61,7 @@ const color_update = asyncHandler(async (req, res) => {
 
 const colorController = {
   colors_list,
+  color_get,
   color_create,
   color_update,
 };
