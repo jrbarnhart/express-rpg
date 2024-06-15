6735c88005ffe21a8b301a710669cf3a96679da8
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-14 21:06:32 -0400

    Add idArray schema

--- src/lib/zod/Global.ts
+++ src/lib/zod/Global.ts
@@ -4,3 +4,5 @@ export const idSchema = z
   .number()
   .int({ message: "Species ID must be an integer" })
   .positive({ message: "Species ID must be positive." });
+
+export const idArraySchema = z.array(idSchema).nonempty;
