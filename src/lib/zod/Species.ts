d543f4900c9da0f90554c03967742613e13af2a9
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-14 21:42:28 -0400

    Update zod species schema with base stats

--- src/lib/zod/Species.ts
+++ src/lib/zod/Species.ts
@@ -1,12 +1,16 @@
 import { z } from "zod";
-import { idArraySchema, name32Schema } from "./Global";
+import { attributeSchema, idArraySchema, name32Schema } from "./Global";
 
 export const CreateSpeciesSchema = z.object({
   name: name32Schema,
   colorIds: idArraySchema,
+  baseHealth: attributeSchema,
+  baseMood: attributeSchema,
 });
 
 export const UpdateSpeciesSchema = z.object({
   name: name32Schema.optional(),
   colorIds: idArraySchema.optional(),
+  baseHealth: attributeSchema.optional(),
+  baseMood: attributeSchema.optional(),
 });
