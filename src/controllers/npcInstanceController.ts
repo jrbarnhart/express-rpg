748d92552d119053e1f16e3d80446c05c8392acf
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-17 17:53:16 -0400

    Return new ncp instance on successful creation

--- src/controllers/npcInstanceController.ts
+++ src/controllers/npcInstanceController.ts
@@ -2,6 +2,9 @@ import asyncHandler from "express-async-handler";
 import sendErrorResponse from "../lib/controllerUtils/sendErrorResponse";
 import sendResponse from "../lib/controllerUtils/sendResponse";
 import npcInstanceQueries from "../lib/prisma/queries/npcInstanceQueries";
+import validateRequestData from "../lib/zod/validateRequestData";
+import { CreateNpcInstanceSchema } from "../lib/zod/NpcInstance";
+import handlePrismaError from "../lib/prisma/handlePrismaError";
 
 const npc_instances_list = asyncHandler(async (req, res) => {
   const allNpcInstances = await npcInstanceQueries.list();
@@ -21,7 +24,16 @@ const npc_instance_get = asyncHandler(async (req, res) => {
 });
 
 const npc_instance_create = asyncHandler(async (req, res) => {
-  sendErrorResponse(res, "NYI");
+  const data = validateRequestData(req.body.data, res, CreateNpcInstanceSchema);
+
+  if (!data) return;
+
+  try {
+    const newNpcInstance = await npcInstanceQueries.create(data);
+    sendResponse(res, "New Npc Instance created.", newNpcInstance);
+  } catch (error) {
+    handlePrismaError(error, res, "Error while creating Npc Instance.");
+  }
 });
 
 const npc_instance_update = asyncHandler(async (req, res) => {
