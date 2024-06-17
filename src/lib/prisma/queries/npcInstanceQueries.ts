43751dd27a41b91c049208e0fe255cca3c69b2f6
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-17 17:49:17 -0400

    Add query for creating instance

--- src/lib/prisma/queries/npcInstanceQueries.ts
+++ src/lib/prisma/queries/npcInstanceQueries.ts
@@ -1,4 +1,6 @@
 import prisma from "../prisma";
+import { z } from "zod";
+import { CreateNpcInstanceSchema } from "../../zod/NpcInstance";
 
 const list = () => {
   return prisma.npcInstance.findMany();
@@ -8,6 +10,18 @@ const findById = (id: number) => {
   return prisma.npcInstance.findUnique({ where: { id: id } });
 };
 
-const npcInstanceQueries = { list, findById };
+const create = (data: z.infer<typeof CreateNpcInstanceSchema>) => {
+  return prisma.npcInstance.create({
+    data: {
+      name: data.name,
+      template: { connect: { id: data.templateId } },
+      health: data.health,
+      mood: data.mood,
+      ...(data.battleId ? { battle: { connect: { id: data.battleId } } } : {}),
+    },
+  });
+};
+
+const npcInstanceQueries = { list, findById, create };
 
 export default npcInstanceQueries;
