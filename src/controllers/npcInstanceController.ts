649ff0231310d538c480263d2524cfff30b0c8ca
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-17 18:05:48 -0400

    Add method logic for updating instance

--- src/controllers/npcInstanceController.ts
+++ src/controllers/npcInstanceController.ts
@@ -3,13 +3,16 @@ import sendErrorResponse from "../lib/controllerUtils/sendErrorResponse";
 import sendResponse from "../lib/controllerUtils/sendResponse";
 import npcInstanceQueries from "../lib/prisma/queries/npcInstanceQueries";
 import validateRequestData from "../lib/zod/validateRequestData";
-import { CreateNpcInstanceSchema } from "../lib/zod/NpcInstance";
+import {
+  CreateNpcInstanceSchema,
+  UpdateNpcInstanceSchema,
+} from "../lib/zod/NpcInstance";
 import handlePrismaError from "../lib/prisma/handlePrismaError";
 
 const npc_instances_list = asyncHandler(async (req, res) => {
   const allNpcInstances = await npcInstanceQueries.list();
 
-  sendResponse(res, "All npc instances retrieved.", allNpcInstances);
+  sendResponse(res, "All NPC instances retrieved.", allNpcInstances);
 });
 
 const npc_instance_get = asyncHandler(async (req, res) => {
@@ -17,9 +20,9 @@ const npc_instance_get = asyncHandler(async (req, res) => {
   const foundInstance = await npcInstanceQueries.findById(id);
 
   if (!foundInstance) {
-    sendErrorResponse(res, "Npc instance not found.");
+    sendErrorResponse(res, "NPC instance not found.");
   } else {
-    sendResponse(res, "Npc instance retrieved successfully.", foundInstance);
+    sendResponse(res, "NPC instance retrieved successfully.", foundInstance);
   }
 });
 
@@ -30,14 +33,25 @@ const npc_instance_create = asyncHandler(async (req, res) => {
 
   try {
     const newNpcInstance = await npcInstanceQueries.create(data);
-    sendResponse(res, "New Npc Instance created.", newNpcInstance);
+    sendResponse(res, "New Npc instance created.", newNpcInstance);
   } catch (error) {
-    handlePrismaError(error, res, "Error while creating Npc Instance.");
+    handlePrismaError(error, res, "Error while creating NPC instance.");
   }
 });
 
 const npc_instance_update = asyncHandler(async (req, res) => {
-  sendErrorResponse(res, "NYI");
+  const data = validateRequestData(req.body.data, res, UpdateNpcInstanceSchema);
+
+  if (!data) return;
+
+  const id = parseInt(req.params.id);
+
+  try {
+    const updatedNpcInstance = await npcInstanceQueries.update(id, data);
+    sendResponse(res, "NPC instance updated successfully.", updatedNpcInstance);
+  } catch (error) {
+    handlePrismaError(error, res, "Error while updating NPC instance.");
+  }
 });
 
 const npcInstanceController = {
