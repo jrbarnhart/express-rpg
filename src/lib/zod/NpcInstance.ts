6f0545b4aeecb45f87591abbacfa954242c21b7e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-17 17:06:17 -0400

    Add schemas for creating and updating instances

---
+++ src/lib/zod/NpcInstance.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+import { attributeSchema, idSchema, name64Schema } from "./Global";
+
+export const CreateNpcInstanceSchema = z.object({
+  name: name64Schema,
+  templateId: idSchema,
+  health: attributeSchema,
+  mood: attributeSchema,
+  battleId: attributeSchema.optional(),
+});
+
+export const UpdateNpcInstanceSchema = z.object({
+  name: name64Schema.optional(),
+  templateId: idSchema.optional(),
+  health: attributeSchema.optional(),
+  mood: attributeSchema.optional(),
+  battleId: attributeSchema.optional(),
+});
