ca7f2e85541ae1a33157ab49736043ee28e6e7a6
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-17 16:53:08 -0400

    Remove async await since it is used in async controller fn

--- src/lib/prisma/queries/npcInstanceQueries.ts
+++ src/lib/prisma/queries/npcInstanceQueries.ts
@@ -1,7 +1,7 @@
 import prisma from "../prisma";
 
-const list = async () => {
-  return await prisma.npcInstance.findMany();
+const list = () => {
+  return prisma.npcInstance.findMany();
 };
 
 const npcInstanceQueries = { list };
