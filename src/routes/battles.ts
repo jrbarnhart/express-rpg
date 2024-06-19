94cd46bd1800cc583edd9ffcc49b1c1fa2b08a19
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-19 14:28:36 -0400

    Add routes for pve battles

---
+++ src/routes/battles.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import isAuthenticated from "../middleware/auth/isAuthenticated";
+import pveBattleController from "../controllers/pveBattleController";
+const battlesRouter = express.Router();
+
+// PvE Battles
+battlesRouter.get("/pve/:id", pveBattleController.pve_battle_get);
+
+battlesRouter.put(
+  "/pve/:id",
+  isAuthenticated(),
+  pveBattleController.pve_battle_update
+);
+
+battlesRouter.post(
+  "/pve/:id",
+  isAuthenticated(),
+  pveBattleController.pve_battle_action
+);
+
+battlesRouter.get("/pve", pveBattleController.pve_battle_list);
+
+battlesRouter.post(
+  "/pve",
+  isAuthenticated(),
+  pveBattleController.pve_battle_create
+);
