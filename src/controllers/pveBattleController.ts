26314325d2286dbc1366e06cca2ad26d85e720eb
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-19 15:17:14 -0400

    Add handlers for get and list

--- src/controllers/pveBattleController.ts
+++ src/controllers/pveBattleController.ts
@@ -1,12 +1,21 @@
 import asyncHandler from "express-async-handler";
 import sendResponse from "../lib/controllerUtils/sendResponse";
+import pveBattleQuery from "../lib/prisma/queries/pveBattlesQuery";
+import sendErrorResponse from "../lib/controllerUtils/sendErrorResponse";
 
 const pve_battle_list = asyncHandler(async (req, res) => {
-  sendResponse(res, "NYI");
+  const allPveBattles = await pveBattleQuery.list();
+  sendResponse(res, "All PvE battles retrieved successfully.", allPveBattles);
 });
 
 const pve_battle_get = asyncHandler(async (req, res) => {
-  sendResponse(res, "NYI");
+  const id = parseInt(req.params.id);
+  const foundPveBattle = await pveBattleQuery.findById(id);
+  if (!foundPveBattle) {
+    sendErrorResponse(res, "PvE battle not found.");
+  } else {
+    sendResponse(res, "PvE battle retrieved successfully.", foundPveBattle);
+  }
 });
 
 const pve_battle_create = asyncHandler(async (req, res) => {
