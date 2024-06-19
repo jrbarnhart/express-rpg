53c172518cc809386ef24cfed9daee3cb41e786c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-19 18:34:58 -0400

    Add method for creating new pve battle

--- src/controllers/pveBattleController.ts
+++ src/controllers/pveBattleController.ts
@@ -2,6 +2,10 @@ import asyncHandler from "express-async-handler";
 import sendResponse from "../lib/controllerUtils/sendResponse";
 import pveBattleQuery from "../lib/prisma/queries/pveBattlesQuery";
 import sendErrorResponse from "../lib/controllerUtils/sendErrorResponse";
+import selectBattleTemplates from "../lib/controllerUtils/selectBattleTemplates";
+import userQuery from "../lib/prisma/queries/userQuery";
+import { iNewBattleData } from "../lib/types/types";
+import handlePrismaError from "../lib/prisma/handlePrismaError";
 
 const pve_battle_list = asyncHandler(async (req, res) => {
   const allPveBattles = await pveBattleQuery.list();
@@ -19,7 +23,48 @@ const pve_battle_get = asyncHandler(async (req, res) => {
 });
 
 const pve_battle_create = asyncHandler(async (req, res) => {
-  sendResponse(res, "NYI");
+  if (!req.user?.id) {
+    console.error(
+      "Authenticated user's credentials were not found in pve_battle_create. Check auth middleware in route."
+    );
+    sendErrorResponse(res, "User credentials not found.");
+    return;
+  }
+
+  const userId = req.user.id;
+  const user = await userQuery.findById(userId);
+  if (!user) {
+    sendErrorResponse(res, "Cannot create battle. User not found.");
+    return;
+  }
+
+  const activePet = user.pets.find((pet) => pet.isActive);
+  if (!activePet) {
+    sendErrorResponse(
+      res,
+      "Cannot create battle. User does not have an active pet."
+    );
+    return;
+  }
+
+  const targetBattlePower = activePet.health + activePet.mood;
+  const opponentTemplates = await selectBattleTemplates(targetBattlePower);
+  if (!opponentTemplates) {
+    sendErrorResponse(res, "Cannot create battle. Opponents not found.");
+    return;
+  }
+
+  const data: iNewBattleData = {
+    opponentTemplates,
+    userId,
+  };
+
+  try {
+    const newPveBattle = await pveBattleQuery.create(data);
+    sendResponse(res, "PvE battle created successfully.", newPveBattle);
+  } catch (error) {
+    handlePrismaError(error, res, "Error while creating PvE battle.");
+  }
 });
 
 const pve_battle_update = asyncHandler(async (req, res) => {
