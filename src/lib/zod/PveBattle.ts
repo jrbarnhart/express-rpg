3e22c4ebd90871f07ff56d180b2e092719facc61
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-19 15:44:07 -0400

    Remove create schema as it is not needed

--- src/lib/zod/PveBattle.ts
+++ src/lib/zod/PveBattle.ts
@@ -1,9 +1,4 @@
 import { z } from "zod";
-import { CreateNpcInstanceSchema } from "./NpcInstance";
-
-export const CreatePveBattleSchema = z.object({
-  opponents: z.array(CreateNpcInstanceSchema).max(6),
-});
 
 export const UpdatePveBattleSchema = z.object({
   isActive: z.boolean().optional(),
