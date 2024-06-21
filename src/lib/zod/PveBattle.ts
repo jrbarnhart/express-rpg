849558897d5813d415bc4cdeb607bbc8af4560fa
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-21 19:57:26 -0400

    Add action schema

--- src/lib/zod/PveBattle.ts
+++ src/lib/zod/PveBattle.ts
@@ -1,6 +1,30 @@
 import { z } from "zod";
+import { idSchema } from "./Global";
 
 export const UpdatePveBattleSchema = z.object({
   isActive: z.boolean().optional(),
   isVictory: z.boolean().optional(),
 });
+
+enum ACTION_OPTIONS {
+  attack = "attack",
+  defend = "defend",
+  run = "run",
+}
+
+export const ActionSchema = z
+  .object({
+    action: z.nativeEnum(ACTION_OPTIONS, {
+      message: "Action invalid. Valid actions:",
+    }),
+    targetId: idSchema.optional(),
+  })
+  .refine(
+    (data) => {
+      if (data.action === ACTION_OPTIONS.attack && !data.targetId) {
+        return false;
+      }
+      return true;
+    },
+    { message: "You must include a targetId with your attack action." }
+  );
