e6409fa7efae5407470e43d994a18c869d5f67cc
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-22 18:56:37 -0400

    Add util for calculating hit chance

---
+++ src/lib/controllerUtils/battle/calcHitChance.ts
@@ -0,0 +1,13 @@
+const calcHitChance = (
+  attackerAccuracy: number,
+  targetSpeed: number,
+  k = 0.1
+) => {
+  const maxPenalty = 0.5;
+  const exponent = k * (targetSpeed - attackerAccuracy);
+  const sigmoid = 1 / (1 + Math.exp(exponent));
+  const hitChance = 1 - maxPenalty * sigmoid;
+  return hitChance;
+};
+
+export default calcHitChance;
