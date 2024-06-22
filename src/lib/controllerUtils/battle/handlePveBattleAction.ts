192d4c2dc7a20ccd68d87cb1c8b4baadbba8357f
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-22 15:13:37 -0400

    Use z infer type with guard

---
+++ src/lib/controllerUtils/battle/handlePveBattleAction.ts
@@ -0,0 +1,65 @@
+/*     1. Verify target
+    2. Calculate damage and apply to npc instances
+    3. If no foes remain (currentHealth/Mood > 0) then set battle isActive = false 
+       and isVictory = true and send victory response
+    4. For each remaining enemy 
+        a. Determine action (attack, defend)
+        b. Calculate damage
+    5. Apply damage to user pet
+    6. If pet dies (currentHealth/Mood <= 0) then set battle isActive = false
+       and send loss response
+    7. Send default battle response */
+import { PveBattleActionSchema } from "../../zod/PveBattle";
+import { Response } from "express";
+import { PveBattleWithOpponents } from "../../types/types";
+import sendErrorResponse from "../sendErrorResponse";
+import { Pet } from "@prisma/client";
+import sendResponse from "../sendResponse";
+import { z } from "zod";
+
+const attack = (
+  res: Response,
+  data: z.infer<typeof PveBattleActionSchema>,
+  battle: PveBattleWithOpponents,
+  userPet: Pet
+) => {
+  if (!data.targetId) {
+    sendErrorResponse(res, "No target was declared for your attack.");
+    return;
+  }
+
+  const opponentIds: number[] = [];
+  for (const opponent of battle.opponents) {
+    opponentIds.push(opponent.id);
+  }
+
+  if (!opponentIds.includes(data.targetId)) {
+    sendErrorResponse(
+      res,
+      "Cannot attack opponent that is not a part of this battle."
+    );
+    return;
+  }
+
+  // Speed is 75% currentHealth and 25% currentMood + 75% health and 25% mood
+  const petSpeed =
+    0.75 * userPet.health +
+    0.3725 * userPet.currentHealth +
+    0.25 * userPet.mood +
+    0.1225 * userPet.currentMood;
+  // Accuracy is 25/75 as above
+  // Power is 50/50 as above
+  // Determine attack order based on speed
+  // Apply attacks. Can only attack if health and mood > 0
+  // Can only attack target with health and mood > 0
+  // Add attacks and results to log as they happen
+  //
+
+  sendResponse(res, "Attack successful!", petSpeed);
+};
+
+const handlePveBattleAction = {
+  attack,
+};
+
+export default handlePveBattleAction;
