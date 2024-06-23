e85dbfb7a128a99ef4336e67e372b3a5e2cb5264
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-23 13:58:47 -0400

    Add helper for calcing damage

--- src/lib/controllerUtils/battle/actionHelpers.ts
+++ src/lib/controllerUtils/battle/actionHelpers.ts
@@ -1,17 +1,22 @@
-const calcHitChance = (
-  attackerAccuracy: number,
-  targetSpeed: number,
-  k = 0.1
-) => {
+const calcHit = (attackerAccuracy: number, targetSpeed: number, k = 0.1) => {
   const maxPenalty = 0.5;
   const exponent = k * (targetSpeed - attackerAccuracy);
   const sigmoid = 1 / (1 + Math.exp(exponent));
   const hitChance = 1 - maxPenalty * sigmoid;
-  return hitChance;
+  return Math.random() < hitChance;
+};
+
+const calcDamage = (attackerPower: number) => {
+  const critChance = 0.5;
+  const powerMod = 0.2;
+  const didCrit = Math.random() < critChance;
+  const critMod = didCrit ? 1.5 : 1;
+  return { damage: attackerPower * powerMod * critMod, didCrit };
 };
 
 const actionHelpers = {
-  calcHitChance,
+  calcHit,
+  calcDamage,
 };
 
 export default actionHelpers;
