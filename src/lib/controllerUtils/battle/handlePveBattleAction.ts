f1e1b4cd0bf8309f8dbb5d49553f6ed7ba9c5cb9
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-23 19:38:23 -0400

    Add comment to outline future structure

--- src/lib/controllerUtils/battle/handlePveBattleAction.ts
+++ src/lib/controllerUtils/battle/handlePveBattleAction.ts
@@ -9,7 +9,7 @@
     6. If pet dies (currentHealth/Mood <= 0) then set battle isActive = false
        and send loss response
     7. Send default battle response */
-import { PveBattleActionSchema } from "../../zod/PveBattle";
+import { ACTION_OPTIONS, PveBattleActionSchema } from "../../zod/PveBattle";
 import { Response } from "express";
 import { PveBattleWithOpponents } from "../../types/types";
 import sendErrorResponse from "../sendErrorResponse";
@@ -42,7 +42,7 @@ const handlePveBattleAction = async (
 
   const allStats = [...opponentStats, petStats];
 
-  const actionOrder = calcBattle.actionOrderById(allStats);
+  const actorOrder = calcBattle.actorOrder(allStats);
 
   const targetInfo = verifyTarget(res, battle, data, opponentStats);
   if (!targetInfo) return;
@@ -59,39 +59,57 @@ const handlePveBattleAction = async (
   );
   allActors[petComparisonId] = userPet;
 
-  // Apply actions. Can only act if health and mood > 0
-  for (const actorId of actionOrder) {
+  for (const actorId of actorOrder) {
     log.push(`${allActors[actorId].name}'s turn:`);
+    // Can only act if health and mood > 0
     if (
       allActors[actorId].currentHealth <= 0 ||
       allActors[actorId].currentMood <= 0
     ) {
       continue;
     }
+
+    /*    Replace data.action with the determiend action 
+      if (
+      data.action === ACTION_OPTIONS.attack ||
+      data.action === ACTION_OPTIONS.insult
+    ) {
+      // Handle attack
+      // Handle insult
+      continue;
+    }
+
+    if (data.action === ACTION_OPTIONS.defend) {
+      // Handle defend
+      continue;
+    }
+
+    if (data.action === ACTION_OPTIONS.run) {
+      // Handle run
+    } */
+
     if (actorId === petComparisonId) {
-      if (userPet.currentHealth > 0 && userPet.currentMood > 0) {
-        // calc damage to target
-        const didHit = calcBattle.hit(petStats.accuracy, targetStats.speed);
+      // calc damage to target
+      const didHit = calcBattle.hit(petStats.accuracy, targetStats.speed);
 
-        const { damage, didCrit } = calcBattle.damage(petStats.power);
+      const { damage, didCrit } = calcBattle.damage(petStats.power);
 
-        const newCurrentHealth = Math.min(target.currentHealth - damage, 0);
-        // log
-        log.push(
-          `Your pet attacked ${target.name}. ${
-            didHit
-              ? `It ${
-                  didCrit ? "CRITICALLY " : ""
-                }hit and did ${damage} health damage.`
-              : "It missed..."
-          }`
-        );
-        /* const updatedTarget = await npcInstanceQuery.update(data.targetId, {
+      const newCurrentHealth = Math.min(target.currentHealth - damage, 0);
+      // log
+      log.push(
+        `Your pet attacked ${target.name}. ${
+          didHit
+            ? `It ${
+                didCrit ? "CRITICALLY " : ""
+              }hit and did ${damage} health damage.`
+            : "It missed..."
+        }`
+      );
+      /* const updatedTarget = await npcInstanceQuery.update(data.targetId, {
           currentHealth: newCurrentHealth,
         }); */
-      }
     } else {
-      // Do npc attack
+      // Do npc action
     }
   }
 
