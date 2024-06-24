bd167e7281547cffc442a186235884a2b945e170
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-24 03:33:59 -0400

    Return the log data

--- src/lib/controllerUtils/battle/battleLog.ts
+++ src/lib/controllerUtils/battle/battleLog.ts
@@ -1,7 +1,7 @@
 import { ActorWithAction } from "../../types/types";
 
 const battleLog = () => {
-  const log: string[] = [];
+  const data: string[] = [];
 
   const actorName = (actor: ActorWithAction) => {
     const isPet = "color" in actor;
@@ -9,19 +9,20 @@ const battleLog = () => {
   };
 
   return {
+    data,
     actorDead: (actor: ActorWithAction) => {
-      log.push(`It looks like ${actorName(actor)} isn't breathing...`);
+      data.push(`It looks like ${actorName(actor)} isn't breathing...`);
     },
     actorMindless: (actor: ActorWithAction) => {
-      log.push(
+      data.push(
         `It looks like ${actorName(actor)} isn't aware of its surroundings...`
       );
     },
     actorAshes: (actor: ActorWithAction) => {
-      log.push(`All that remains of ${actorName(actor)} is a pile of ash...`);
+      data.push(`All that remains of ${actorName(actor)} is a pile of ash...`);
     },
     actorTurn: (actor: ActorWithAction) => {
-      log.push(`${actorName(actor)} prepares to act.`);
+      data.push(`${actorName(actor)} prepares to act.`);
     },
     actorAttacked: (
       actor: ActorWithAction,
@@ -30,7 +31,7 @@ const battleLog = () => {
       didCrit: boolean,
       damage: number
     ) => {
-      log.push(
+      data.push(
         `${actorName(actor)} attacked ${actorName(target)}. ${
           didHit
             ? `It ${
