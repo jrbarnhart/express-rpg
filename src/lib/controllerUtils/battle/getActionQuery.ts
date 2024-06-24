cb2c2a03a2df53d0d240f09563733aee30ec9701
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-06-24 14:24:38 -0400

    Add methods for defend and insult

--- src/lib/controllerUtils/battle/getActionQuery.ts
+++ src/lib/controllerUtils/battle/getActionQuery.ts
@@ -18,8 +18,53 @@ const forAttack = (
   return npcInstanceQuery.update(actor.id, { currentHealth: newHealth });
 };
 
+const forInsult = (
+  actor: ActorWithAction,
+  damage: number,
+  petComparisonId: number
+) => {
+  const isUserPet = actor.id === petComparisonId;
+
+  const newMood = Math.min(actor.currentMood - damage, 0);
+
+  if (isUserPet) {
+    return petQuery.update(actor.id, { currentMood: newMood });
+  }
+
+  return npcInstanceQuery.update(actor.id, { currentMood: newMood });
+};
+
+const forDefend = (
+  actor: ActorWithAction,
+  recoveryAmount: number,
+  petComparisonId: number
+) => {
+  const isUserPet = actor.id === petComparisonId;
+
+  const newHealth = Math.min(
+    actor.currentHealth + recoveryAmount,
+    actor.health
+  );
+
+  const newMood = Math.min(actor.currentMood + recoveryAmount, actor.mood);
+
+  if (isUserPet) {
+    return petQuery.update(actor.id, {
+      currentHealth: newHealth,
+      currentMood: newMood,
+    });
+  }
+
+  return npcInstanceQuery.update(actor.id, {
+    currentHealth: newHealth,
+    currentMood: newMood,
+  });
+};
+
 const getActionQuery = {
   forAttack,
+  forInsult,
+  forDefend,
 };
 
 export default getActionQuery;
